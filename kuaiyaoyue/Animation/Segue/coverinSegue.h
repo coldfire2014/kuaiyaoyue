b1ba6b99cdfd97b75630a001fcc6bcbe06b8dfd9
coldfire2014 <iphone4wyb@gmail.com>
2014-12-08 18:01:38 +0800

    adds

---
+++ kuaiyaoyue/Animation/Segue/coverinSegue.h
@@ -0,0 +1,13 @@
+//
+//  coverinSegue.h
+//  kuaiyaoyue
+//
+//  Created by wuyangbing on 14/12/8.
+//  Copyright (c) 2014年 davidwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface coverinSegue : UIStoryboardSegue
+
+@end
---
+++ kuaiyaoyue/Animation/Segue/coverinSegue.m
@@ -0,0 +1,13 @@
+//
+//  coverinSegue.m
+//  kuaiyaoyue
+//
+//  Created by wuyangbing on 14/12/8.
+//  Copyright (c) 2014年 davidwang. All rights reserved.
+//
+
+#import "coverinSegue.h"
+
+@implementation coverinSegue
+
+@end
