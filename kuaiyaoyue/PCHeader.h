93c27791a366e0cddd4c262f4e15ab2f10240845
coldfire2014 <iphone4wyb@gmail.com>
2015-03-05 11:56:08 +0800

    qiniu

--- kuaiyaoyue/PCHeader.h
+++ kuaiyaoyue/PCHeader.h
@@ -12,7 +12,7 @@
 #define ISIOS7LATER [[[UIDevice currentDevice] systemVersion] floatValue]>=7
 #define ISIOS8LATER [[[UIDevice currentDevice] systemVersion] floatValue]>=8
 #define QNTOKEN @"http://appkyy.kyy121.com/oss/upload/getClientDto"
-#define QNPCI @"http://7vili3.com1.z0.glb.clouddn.com/"
+#define QNPCI @"http://7vili3.com2.z0.glb.qiniucdn.com/"
 //#define YINGLOU @"YINGLOU"
 #ifdef YINGLOU
     #define YINGLOUURL @"http://appkyy.kyy121.com/studio/"
